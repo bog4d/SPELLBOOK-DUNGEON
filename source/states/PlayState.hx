f8dd7f630921d97304203ad0316467e7fe4d3e58
B4D <84340860+bog4d@users.noreply.github.com>
2024-06-10 22:07:23 +0300

    Cool crosshair line

--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -6,6 +6,7 @@ import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxState;
 import flixel.math.FlxMath;
+import flixel.util.FlxSpriteUtil;
 
 class PlayState extends FlxState
 {
@@ -17,6 +18,7 @@ class PlayState extends FlxState
 	var player:Player;
 
 	var crosshair:FlxSprite;
+	var crosshairLine:FlxSprite;
 
 	override public function create()
 	{
@@ -35,6 +37,10 @@ class PlayState extends FlxState
 		player = new Player();
 		player.screenCenter();
 
+		crosshairLine = new FlxSprite();
+		crosshairLine.makeGraphic(FlxG.width, FlxG.height, 0, true);
+		crosshairLine.antialiasing = true;
+
 		crosshair = new FlxSprite('assets/images/crosshair.png');
 		crosshair.antialiasing = true;
 		crosshair.scale.set(.8, .8);
@@ -42,6 +48,7 @@ class PlayState extends FlxState
 		crosshair.setPosition(FlxG.mouse.getWorldPosition(camGame).x, FlxG.mouse.getWorldPosition(camGame).y);
 
 		//-----[Layering]-----\\
+		add(crosshairLine);
 		add(player);
 		add(crosshair);
 		//---------------------\\
@@ -69,5 +76,19 @@ class PlayState extends FlxState
 		crosshair.y = FlxMath.lerp(crosshair.y, crosshairLerpY, 15 * elapsed);
 
 		crosshair.angle += 25 * elapsed;
+
+		FlxSpriteUtil.fill(crosshairLine, 0);
+
+		FlxSpriteUtil.drawLine(crosshairLine, player.x
+			+ player.width / 2, player.y
+			+ player.height / 2, crosshair.x
+			+ crosshair.origin.x,
+			crosshair.y
+			+ crosshair.origin.y, {
+				thickness: 3,
+				color: 0xFF872341
+			});
+
+		crosshairLine.setPosition(camGame.scroll.x, camGame.scroll.y);
 	}
 }
