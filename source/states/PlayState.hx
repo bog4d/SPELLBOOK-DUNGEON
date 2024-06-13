61a6d21698af71b354d4f6e5199add7502b22611
B4D <84340860+bog4d@users.noreply.github.com>
2024-06-13 12:45:41 +0300

    Level prefabs wip! Cheem go crazy

--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -10,7 +10,10 @@ import flixel.FlxSprite;
 import flixel.FlxState;
 import flixel.addons.display.FlxBackdrop;
 import flixel.addons.display.FlxGridOverlay;
+import flixel.addons.editors.ogmo.FlxOgmo3Loader;
+import flixel.group.FlxGroup.FlxTypedGroup;
 import flixel.math.FlxMath;
+import flixel.tile.FlxTilemap;
 import flixel.tweens.FlxEase;
 import flixel.tweens.FlxTween;
 import flixel.util.FlxSpriteUtil;
@@ -30,11 +33,11 @@ class PlayState extends FlxState
 	public var plrHurtbox:FlxObject;
 
 	var spellCastTxt:SpellCastText;
-
 	var crosshair:FlxSprite;
 	var crosshairLine:FlxSprite;
 
-	private var bg:FlxBackdrop;
+	var prefabLoader:FlxOgmo3Loader;
+	var prefabGrp:FlxTypedGroup<FlxTilemap>;
 
 	public static var unlockedSpells:Map<SPELLS_ACTION, Bool> = [
 		EXPLOSION => false, HEAL => false, POISON => false, TELEPORT => false, SPEED_BOOST => false, BURST => false, PIERCER => false, BOUNCE => false,
@@ -54,7 +57,16 @@ class PlayState extends FlxState
 		FlxG.cameras.add(camHud);
 
 		camHud.bgColor.alpha = 0;
-		//--------------------\\
+		//-----[LV LOADER SHITS]-----\\
+		prefabLoader = new FlxOgmo3Loader('assets/data/SpellbookDungeon.ogmo', 'assets/data/lv_prefabs/start.json');
+		var tilemap:FlxTilemap = prefabLoader.loadTilemap('assets/images/tileset.png', 'Level');
+		prefabGrp = new FlxTypedGroup<FlxTilemap>();
+		prefabGrp.add(tilemap);
+
+		for (prefab in prefabGrp.members)
+			addTileProprieties(prefab); // read the func comment, vic
+
+		//----------------------------\\
 		hud = new HUD();
 		hud.cameras = [camHud];
 
@@ -76,15 +88,14 @@ class PlayState extends FlxState
 		crosshair.setPosition(FlxG.mouse.getWorldPosition(camGame).x, FlxG.mouse.getWorldPosition(camGame).y);
 
 		final gridGraphic = FlxGridOverlay.createGrid(64, 64, 128, 128, true, 0xFF0E0E0E, 0xFF222222);
-		bg = new FlxBackdrop(gridGraphic, XY);
+		var bg = new FlxBackdrop(gridGraphic, XY);
 		bg.scrollFactor.set(.3, .3);
 		bg.velocity.set(15, 15);
 		bg.color = 0xFF393939;
 
 		//-----[Layering]-----\\
 		add(bg);
-		add(new FlxSprite(FlxG.width / 2, FlxG.height / 2, 'assets/images/poorCheem.png'));
-		// add(crosshairLine); pretty broken sorry
+		add(prefabGrp); // add(crosshairLine); pretty broken sorry
 		add(new SpellBook(HEAL));
 
 		add(player);
@@ -105,8 +116,13 @@ class PlayState extends FlxState
 	override public function update(elapsed:Float)
 	{
 		super.update(elapsed);
+		FlxG.collide(player, prefabGrp);
+
 		camGame.followLerp = 5 * elapsed;
 		plrHurtbox.setPosition(player.x + 15, player.y - 125);
+
+		// THIS IS BAD. I HATE THIS. CHEEM. HELP. CHEEEEM. CHEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEM
+		camGame.setScrollBoundsRect(plrHurtbox.x - FlxG.width, plrHurtbox.y - FlxG.height, 128000, 72000, true);
 
 		spellCastTxt.setPosition(player.x + player.width / 2, player.y - 200);
 
@@ -261,5 +277,29 @@ class PlayState extends FlxState
 		{
 			spell = false;
 		}
+	}
+
+	// VIC! Read this: Im guessing since we'll have multiple tilemaps, we still need to
+	// add tile proprieties to each one of them. So just use this function on every one of them-
+	// oooor if you have a better idea, be my guest! As long as it makes ur job easier...
+	private function addTileProprieties(tilemap:FlxTilemap):Void
+	{
+		tilemap.setTileProperties(1, NONE);
+		tilemap.setTileProperties(3, ANY);
+		tilemap.setTileProperties(4, ANY);
+		tilemap.setTileProperties(5, ANY);
+		tilemap.setTileProperties(11, ANY);
+		tilemap.setTileProperties(12, ANY);
+		tilemap.setTileProperties(13, ANY);
+
+		tilemap.setTileProperties(8, NONE);
+		tilemap.setTileProperties(9, NONE);
+		tilemap.setTileProperties(10, NONE);
+		tilemap.setTileProperties(16, NONE);
+		tilemap.setTileProperties(17, ANY); // INVISIBLE TO THE PLAYER!!
+		tilemap.setTileProperties(18, NONE);
+		tilemap.setTileProperties(24, NONE);
+		tilemap.setTileProperties(25, NONE);
+		tilemap.setTileProperties(26, NONE);
 	}
 }
